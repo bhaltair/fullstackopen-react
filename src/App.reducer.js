b5cc652e980ecbc1c9c4886e2fc974eebd0b9273
wangqi <njcit_wangqi@163.com>
2020-07-10 21:49:27 +0800

    feat: combineReducers

--- src/App.reducer.js
+++ src/App.reducer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
 
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import './index.css';
 import App from './App.reducer';
 import * as serviceWorker from './serviceWorker';
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { createNote } from './reducers/noteReducer'
+import filterReducer, { filterChange } from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+const store = createStore(reducer, composeWithDevTools())
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+
+
+console.log(store.getState())
+
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+renderApp()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(storeNow)
+  // renderApp()
+})
